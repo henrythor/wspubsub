43cf1b8d97b3b0ad643d7216d879ec12f159cb59
henrythor <henry.baldursson@gmail.com>
2015-08-17 04:59:01 +0000

    Debugging output

--- src/wspubsub_srv.erl
+++ src/wspubsub_srv.erl
@@ -24,22 +24,32 @@ init(Args) ->
     end.
 
 handle_call('topic going down', {Pid, _T}, State) when Pid =:= State#srv.owner ->
-    lager:info("~s: Owner killing topic ~p", [?MODULE, State#srv.topic]),
+    lager:info("~s/~p: Owner killing topic", [?MODULE, State#srv.topic]),
     {stop, normal, State};
 handle_call('add subscriber', {Pid, _T}, State) ->
+    lager:debug("~s/~p: Adding ~p as a sub", [?MODULE, State#srv.topic, Pid]),
+    lager:debug("~s/~p: subs before: ~p",
+        [?MODULE, State#srv.topic, State#srv.subs]),
     NewSubs = State#srv.subs ++ [Pid],
     NewState = State#srv{subs = NewSubs},
+    lager:debug("~s/~p: subs after: ~p", [?MODULE, State#srv.topic, NewSubs]),
     {reply, ok, NewState};
 handle_call('remove subscriber', {Pid, _T}, State) ->
+    lager:debug("~s/~p: Removing ~p from subs",
+        [?MODULE, State#srv.topic, Pid]),
+    lager:debug("~s/~p: subs before: ~p",
+        [?MODULE, State#srv.topic, State#srv.subs]),
     NewSubs = State#srv.subs -- [Pid],
     NewState = State#srv{subs = NewSubs},
+    lager:debug("~s/~p: subs after: ~p", [?MODULE, State#srv.topic, NewSubs]),
     {reply, ok, NewState};
 handle_call({'send', Message}, {Pid, _T}, State) when Pid =:= State#srv.owner ->
     lager:debug("~s: sending ~p to all subs", [?MODULE, Message]),
     send_to_all(State#srv.subs, {'topic message', self(), Message}),
     {reply, ok, State};
 handle_call(Request, From, State) ->
-    lager:error("~s: Ignored call to ~p from ~p", [?MODULE, Request, From]),
+    lager:error("~s/~p: Ignored call to ~p from ~p",
+        [?MODULE, State#srv.topic, Request, From]),
     {reply, ignored, State}.
 
 handle_cast(_Request, State) ->
@@ -51,7 +61,7 @@ handle_info(_Info, State) ->
 terminate(Reason, State) ->
     % From 'global' manual: If a process with a registered name dies, or the
     % node goes down, the name is unregistered on all nodes.
-    lager:info("~s: topic ~p dying: ~p", [?MODULE, State#srv.topic, Reason]),
+    lager:info("~s/~p: dying, ~p", [?MODULE, State#srv.topic, Reason]),
     State#srv.owner ! {'topic going down', self()},
     send_to_all(State#srv.subs, {'topic going down', self()}),
     ok.
