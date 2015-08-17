14c4a5b791ee7cb8fe5ec6bfe16776920152ff23
henrythor <henry.baldursson@gmail.com>
2015-08-17 09:32:27 +0000

    using macros instead of patternmatching

--- test/apikeys_tests.erl
+++ test/apikeys_tests.erl
@@ -2,9 +2,9 @@
 -include_lib("eunit/include/eunit.hrl").
 
 verify_new_topic_test() ->
-    true = apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
+    ?assert(apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
                             <<"test">>,
-                            <<"">>),
-    false = apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
+                            <<"">>)),
+    ?assertNot(apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
                              <<"nontest">>,
-                             <<"">>).
+                             <<"">>)).
