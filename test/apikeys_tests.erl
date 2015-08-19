f949f9becd9d420a12a3bb4408240e2593f724a5
henrythor <henry.baldursson@gmail.com>
2015-08-19 02:28:02 +0000

    Test update

--- test/apikeys_tests.erl
+++ test/apikeys_tests.erl
@@ -1,10 +1,6 @@
 -module(apikeys_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-verify_new_topic_test() ->
-    ?assert(apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
-                            <<"test">>,
-                            <<"">>)),
-    ?assertNot(apikeys:verify_new_topic(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,
-                             <<"nontest">>,
-                             <<"">>)).
+verify_test() ->
+    ?assert(apikeys:verify(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,<<"test">>)),
+    ?assertNot(apikeys:verify(<<"f47ac10b-58cc-4372-a567-0e02b2c3d479">>,<<"nontest">>)).
